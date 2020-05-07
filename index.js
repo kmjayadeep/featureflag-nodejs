66a206728581c0173b20702205271abd1ff29819
Jayadeep KM <kmjayadeep@gmail.com>
2020-05-07 22:34:05 +0530

    read config from file

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-
+const fs = require('fs');
 const dotenv = require('dotenv');
+const debug = require('debug')('featureflag-nodejs');
 
 
 
@@ -8,22 +9,53 @@ class FeatureFlags {
 
   // default options: 
   // {
-  //   configFile: 'features.js',
+  //   configFile: './features.js',
   //   envPrefix: 'FEATURE_'
   // }
   //
 
-  #features = {};
-
   constructor(options= {}) {
     dotenv.config(); // use .env file to populate environment variables
-    const { configFile } = options.configFile || 'features';
-    const config = require(configFile);
-    console.log(config)
+    this._features = {};
+
+    const configFile = options.configFile || './features';
+    debug('using config file %o', configFile)
+
+    try{
+      const config = require(configFile);
+      this.loadConfig(config);
+    }catch(err) {
+      debug('config file missing');
+    }
+
+
   }
 
   get features() {
-    return this.#features;
+    return this._features;
+  }
+
+  loadConfig(config) {
+    this._features = {
+      ...this._features,
+      ...config,
+    };
+    debug('loaded config file %o, new feature list is : %o', config, this._features);
+  }
+
+  // made async for future purpose, to support external feature toggling
+  async isEnabled(featureName) {
+    debug('looking for feature %o', featureName);
+    const feature = this._features[featureName];
+    if(typeof feature === 'undefined') {
+      debug('feature is undefined, hence returning false');
+      return false;
+    };
+    if(typeof feature === 'string') {
+      debug('got string value for feature %o', feature);
+      return feature.toLowerCase() === 'true';
+    }
+    return !!feature;
   }
 
 };
--- test/featureflag.spec.js
+++ test/featureflag.spec.js
@@ -1,9 +1,29 @@
+const fs = require('fs');
 const { expect } = require('chai');
+const FeatureFlags = require('../index');
 
 describe('#initialize', () => {
 
   it('should initialize with default config', () => {
+    const featureFlags = new FeatureFlags();
+    expect(featureFlags.features).to.not.null;
+    expect(featureFlags.features).to.not.undefined;
   });
+
+  it.only('should read config file', async () => {
+    fs.writeFileSync('features.json',`
+      {
+        "feature" : "true"
+      }
+    `);
+    const featureFlags = new FeatureFlags();
+    fs.unlinkSync('features.json');
+
+    expect(featureFlags.features).to.not.null;
+    expect(featureFlags.features).to.not.undefined;
+
+    expect(await featureFlags.isEnabled('feature')).to.true;
+  })
 
 });
 
